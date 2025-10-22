304e6a1e35e47cdfd599b94311dc4ea3fd9515ce
Steady-K <tkdlxm56@gmail.com>
2025-10-22 12:59:04 +0900

    feat: add login event handling and user persistence logic to backend

--- utils/io.js
+++ utils/io.js
@@ -1,8 +1,20 @@
+const userController = require("../Controllers/user.controller");
+
 module.exports = function (io) {
   // emit() : 말하는 함수
   // on() : 듣는 함수
   io.on("connection", async (socket) => {
     console.log("client is connected", socket.id);
+
+    socket.on("login", async (userName, cb) => {
+      //유저정보를 저장
+      try {
+        const user = await userController.saveUser(userName, socket.id);
+        cb({ ok: true, data: user });
+      } catch (error) {
+        cb({ ok: false, error: error.message });
+      }
+    });
 
     socket.on("disconnect", () => {
       console.log("user is disconnected");
